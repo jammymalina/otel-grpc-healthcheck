5dedf76a52fda71feb7a15b52c3841a29149438d
jammymalina <jakub.jantosik@gmail.com>
2024-02-05 23:27:00 +0100

    Update

---
+++ generate.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+const fs = require("fs/promises")
+const ejs = require("ejs")
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+
+const argv = yargs(hideBin(process.argv))
+  .options({
+    version: {
+      alias: "v",
+      describe: "provide an otel version",
+      demandOption: true,
+    },
+    goVersion: {
+      alias: "g",
+      describe: "provide a go version",
+      demandOption: true,
+    }
+  })
+  .help()
+  .parse().argv;
+
+const goModTemplateData = await fs.readFile("./go.mod.ejs")
+const goModTemplate = ejs.compile(goModTemplateData)
---
+++ generate_go_mod.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+version=$1
+go_version=$2
+
+git_switch = "!f() { git switch $1 2>/dev/null || git switch -c $1; }; f"
+
+git_switch gen-v${version}
+
+rm go.mod go.sum
+./generate -v ${version} -g ${go_version}
+
+go mod download
+go build
