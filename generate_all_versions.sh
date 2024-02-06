5598f9f32701747d8abddfebe7fc42fc81696355
jammymalina <jakub.jantosik@gmail.com>
2024-02-06 01:18:14 +0100

    Update

---
+++ generate_all_versions.sh
@@ -0,0 +1,21 @@
+!/usr/bin/env bash
+
+set -euxo pipefail
+
+versions=("0.93.0")
+
+for v in ${versions[@]}; do
+  git switch generate-otel-version
+  ./generate_go_mod.sh ${v} "1.20"
+  mv go.mod go.mod.new
+  mv go.sum go.sum.new
+  git switch main
+  mv go.mod.new go.mod
+  mv go.sum.new go.sum
+  git add go.mod go.sum
+  git commit --allow-empty -m "Updated otel to version ${v}"
+  git tag -a "v${v}" -m "v${v}"
+  git push origin HEAD
+  git push origin "v${v}"
+done
+
