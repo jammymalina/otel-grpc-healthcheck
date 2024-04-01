1ab576e7bf99d5b33340317902c90a1661d603cd
jammymalina <jakub.jantosik@gmail.com>
2024-04-01 22:44:48 +0200

    Update

--- auto_generator_service/main.tf
+++ auto_generator_service/main.tf
@@ -25,16 +25,33 @@ provider "aws" {
   }
 }
 
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+data "aws_ssm_parameter" "deploy_key" {
+  name = "/otel-grpc-healthcheck/private_deploy_key"
+}
+
 locals {
   service_name = "otel-package-updater"
 
+  account_id = data.aws_caller_identity.current.account_id
+  region     = data.aws_region.current.name
+
+  log_retention_days = 7
+
+  ecr_repository_url = "${local.account_id}.dkr.ecr.${local.region}.amazonaws.com"
+  build_tag          = "latest"
+
   trigger_lambda_name = "${local.service_name}-trigger"
   dist_trigger_lambda = "dist/handler.zip"
+
+  build_project_name = "${local.service_name}-build"
 }
 
 resource "aws_cloudwatch_log_group" "trigger_lambda" {
   name              = "/aws/lambda/${local.trigger_lambda_name}"
-  retention_in_days = 14
+  retention_in_days = local.log_retention_days
 }
 
 data "aws_iam_policy_document" "lambda_assume_role" {
@@ -78,4 +95,140 @@ resource "aws_lambda_function" "trigger" {
 
   filename = local.dist_trigger_lambda
   runtime  = "nodejs20.x"
+}
+
+resource "aws_ecr_repository" "build" {
+  name                 = "${local.service_name}-build"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "build" {
+  repository = aws_ecr_repository.build.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Expire untagged images older than 3 days"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 3
+        }
+        action = {
+          type = "expire"
+        }
+      },
+    ]
+  })
+}
+
+resource "terraform_data" "build" {
+  triggers_replace = [filesha256("${path.root}/Dockerfile")]
+
+  provisioner "local-exec" {
+    working_dir = path.root
+    interpreter = ["/bin/bash", "-ce"]
+    command     = <<-EOT
+      aws ecr get-login-password --region ${local.region} | docker login --username AWS --password-stdin ${local.ecr_repository_url}
+      docker build \
+	--file Dockerfile \
+	--platform linux/amd64 \
+	--output type=image,name=${aws_ecr_repository.build.repository_url}:${local.build_tag},push=true .
+    EOT
+  }
+}
+
+data "aws_ecr_image" "build" {
+  depends_on = [terraform_data.build]
+
+  repository_name = aws_ecr_repository.build.name
+  image_tag       = local.build_tag
+}
+
+resource "aws_cloudwatch_log_group" "build" {
+  name              = "/codebuild/${local.build_project_name}"
+  retention_in_days = local.log_retention_days
+}
+
+data "aws_iam_policy_document" "build_assume_role" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "build_policy" {
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+    resources = [
+      aws_cloudwatch_log_group.build.arn,
+      "${aws_cloudwatch_log_group.build.arn}:log-stream:*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:GetAuthorizationToken",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    actions   = ["ssm:GetParameter*"]
+    resources = [data.aws_ssm_parameter.deploy_key.arn]
+  }
+}
+
+resource "aws_iam_role" "build" {
+  name               = "${local.build_project_name}-build-role"
+  assume_role_policy = data.aws_iam_policy_document.build_assume_role.json
+
+  inline_policy {
+    name   = "default"
+    policy = data.aws_iam_policy_document.build_policy.json
+  }
+}
+
+resource "aws_codebuild_project" "build" {
+  name          = local.build_project_name
+  build_timeout = 30
+  service_role  = aws_iam_role.build.arn
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "${aws_ecr_repository.build.repository_url}@${data.aws_ecr_image.build.id}"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "SERVICE_ROLE"
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name = aws_cloudwatch_log_group.build.name
+    }
+  }
+
+  source {
+    type      = "NO_SOURCE"
+    buildspec = file("${path.root}/buildspec.yaml")
+  }
 }
